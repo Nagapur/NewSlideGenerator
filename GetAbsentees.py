e43c5c1344384a78f92decd4913a5bc2c89a7c90
Seng Wei <99818793+Nagapur@users.noreply.github.com>
2026-06-23 15:18:44 +0800

    Create GetAbsentees.py

---
+++ GetAbsentees.py
@@ -0,0 +1,32 @@
+!pip install python-pptx
+import pandas as pd
+from pptx import Presentation
+ppt_file_name = 'STE CDCS Promotion Ceremony Deck 2025 v1'
+excel_file = '2025-07-10 CDCS.xlsx'
+excel_file2 = '2025-07-10 CDCS-Diff.xlsx'
+# Load the Excel file with multiple sheets
+xls = pd.ExcelFile(excel_file)
+xls2 = pd.ExcelFile(excel_file2)
+
+#load excel and return a combined dataframe with all sheets
+def loadExcelToCompare(excelFileName):
+    # Read the sheet into a DataFrame
+    df = pd.read_excel(excelFileName, sheet_name=None, engine='openpyxl')
+    combined_df = pd.concat(df.values(), ignore_index=True)
+
+    return combined_df
+
+#Get dataframe that use to do comparison
+fullDf = loadExcelToCompare(excel_file)
+absentDf =  loadExcelToCompare(excel_file2)
+#find the list of absentees by comparing master list vs actual attendance using 'Name'
+missing_names = fullDf.loc[
+    ~fullDf['Name'].str.strip().str.lower().isin(
+        absentDf['Name'].str.strip().str.lower()
+    ),
+]
+
+#Create absentee sheet in excel
+with pd.ExcelWriter("2025-07-10 CDCS-Diff.xlsx", engine="openpyxl", mode="a") as writer:
+    missing_names.to_excel(writer, sheet_name="MSS_Absent", index=False)
+
